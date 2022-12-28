fa456441de8503e48731820e6efba18a67d096cb
karanveersinghA2XYY <121312976+karanveersinghA2XYY@users.noreply.github.com>
2022-12-29 00:40:07 +0530

    😓

--- SpamX/bot/Help.py
+++ SpamX/bot/Help.py
@@ -59,7 +59,7 @@ syntax:
 {hl}hang (counts)
 
 
-**© @RiZoeLX**
+**© @Old_worlds_map**
 """
 
 
@@ -78,7 +78,7 @@ command:
 command:
   {hl}dmraid (count) (username or user id)
 
-**© @RiZoeLX**
+**© @Old_worlds_map**
 """
 
 leave_help = f"""
@@ -89,7 +89,7 @@ syntax:
 i) {hl}leave group Username or chat user id
 ii) {hl}leave
 
-**© @RiZoeLX**
+**© @Old_worlds_map**
 """
 
 userbot_help = f"""
@@ -101,11 +101,11 @@ userbot_help = f"""
 
 - {hl}restart : To Restart Your Spam Bots
 
-**© @RiZoeLX**
+**© @Old_worlds_map**
 """
 
 help_menu = f"""
-**SpamX Help Menu **
+**Apple X Help Menu **
 
 **There are following categories**
 
@@ -120,7 +120,7 @@ help_menu = f"""
 **Type** {hl}help (category) **to get all syntax in that category and its usage**
 **Example**: `{hl}help spam`
 
-**© @RiZoeLX**
+**© @Old_worlds_map**
 """
 
 
@@ -143,5 +143,5 @@ syntax:
 
 **stop cmd**: Simply type {hl}stop to stop spam/abuse/raid any of that 
 
-**© @RiZoeLX**
+**© @Old_worlds_map**
 """
