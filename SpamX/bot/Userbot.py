326bdee2d363f4aed39b5bdc78cf7df72e44ba5e
karanveersinghA2XYY <121312976+karanveersinghA2XYY@users.noreply.github.com>
2022-12-29 00:46:54 +0530

    Update Userbot.py

--- SpamX/bot/Userbot.py
+++ SpamX/bot/Userbot.py
@@ -15,9 +15,9 @@ from pyrogram.types import Message
 from pyrogram import __version__ as pyro_vr             
                 
 
-pongg = PING_MSG if PING_MSG else "Sᴘᴀᴍ X"
-RIZ_PIC = ALIVE_PIC if ALIVE_PIC else "https://telegra.ph/file/ec832fc9107fd21edfee3.jpg"
-Alivemsg = ALIVE_MSG if ALIVE_MSG else "𝗦𝗽𝗮𝗺𝗫 𝗛𝗲𝗿𝗲."
+pongg = PING_MSG if PING_MSG else "𝐊𝐕 -𝐱𝐃 °|"
+RIZ_PIC = ALIVE_PIC if ALIVE_PIC else "https://te.legra.ph/file/6b1e9e0212a148dc381e0.jpg"
+Alivemsg = ALIVE_MSG if ALIVE_MSG else "𝐊𝐕 -𝐱𝐃 °|."
 
 
 rizoel = f"⁂ {Alivemsg} ⁂\n\n"
